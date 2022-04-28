6ca295f00e8d3abc46445e9f664e4f373ff91df4
JialongYin <yinjlusa@gmail.com>
2022-04-28 12:34:30 +0800

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const net = require('net');
 
 function createWindow () {
   // Create the browser window.
@@ -25,7 +26,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
