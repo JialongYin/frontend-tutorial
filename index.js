d3f794b843909f15d62a2af10db3d532451ccb4e
JialongYin <yinjlusa@gmail.com>
2022-04-28 12:29:43 +0800

    first commit

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ function greeting(){
     var input = document.getElementById("myName").value;
 
     document.getElementById("greet_from_server").innerHTML = "PASS HERE";
-    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
-        document.getElementById("greet_from_server").innerHTML = "CREATE CONNECTION";
-        // 'connect' listener.
-        console.log('connected to server!');
-        // send the message
-        client.write(`${input}\r\n`);
-
-        document.getElementById("greet_from_server").innerHTML = "WRITE SUCCESS";
-    });
-
-    // get the data from the server
-    client.on('data', (data) => {
-        document.getElementById("greet_from_server").innerHTML = data;
-        console.log(data.toString());
-        client.end();
-        client.destroy();
-    });
-
-    client.on('end', () => {
-        document.getElementById("greet_from_server").innerHTML = "CLIENT END";
-        console.log('disconnected from server');
-    });
+    // const client = net.createConnection({ port: server_port, host: server_addr }, () => {
+    //     document.getElementById("greet_from_server").innerHTML = "CREATE CONNECTION";
+    //     // 'connect' listener.
+    //     console.log('connected to server!');
+    //     // send the message
+    //     client.write(`${input}\r\n`);
+    //
+    //     document.getElementById("greet_from_server").innerHTML = "WRITE SUCCESS";
+    // });
+    //
+    // // get the data from the server
+    // client.on('data', (data) => {
+    //     document.getElementById("greet_from_server").innerHTML = data;
+    //     console.log(data.toString());
+    //     client.end();
+    //     client.destroy();
+    // });
+    //
+    // client.on('end', () => {
+    //     document.getElementById("greet_from_server").innerHTML = "CLIENT END";
+    //     console.log('disconnected from server');
+    // });
 
 
 }
